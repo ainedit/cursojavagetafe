438da46380b78fc7bd0a889e90846e913def1a70
Tardes <Tardes@GET-PROF>
2025-09-08 17:37:30 +0200

    Ejercicio Biblioteca

--- src/es/cursojava/oo/ejercicios/biblioteca/Main.java
+++ src/es/cursojava/oo/ejercicios/biblioteca/Main.java
@@ -6,11 +6,19 @@ public class Main {
 		Direccion dir1 = new Direccion("Calle1","Ciudad1",12321);
 		Biblioteca biblioteca1 = new Biblioteca("Biblioteca1", dir1, 3);
 		
-		Libro libro1 = new Libro("Titulo","Autor","12312312-123");
+		Libro libro1 = new Libro("Titulo1","Autor1","12312312-123");
 		biblioteca1.agregarLibro(libro1);
 
-		
 		Libro[] librosBiblioteca =  biblioteca1.getLibros();
+		
+		
+		Direccion dir2 = biblioteca1.getDireccion();
+		
+		System.out.println(dir1);
+		System.out.println(dir2);
+		System.out.println(dir1==dir2);
+		
+		biblioteca1.getLibros()[0].getIsbn();
 	}
 
 }
