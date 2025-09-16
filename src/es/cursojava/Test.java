38d2ed0b704d8f07f394c785c4e9a1b9aa8ce3c7
Tardes <Tardes@GET-PROF>
2025-09-16 19:29:15 +0200

    Herencia y Logs

--- src/es/cursojava/Test.java
+++ src/es/cursojava/Test.java
@@ -4,12 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Test {
-	private final static Logger log = (Logger)LoggerFactory.getLogger("HolaMundo");
+	private final static Logger log = LoggerFactory.getLogger(Test.class);
 	
 	public static void main(String[] args) {
 		int num1 = 7;
 		int num2 = 3;
-		log.info("HOla");
+		log.warn("Hola");
 		System.out.println(num1/num2);
 		
 		double num11 = 7;
