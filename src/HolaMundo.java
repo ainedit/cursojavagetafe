59b0fe35ce713463ada1fa690f625a3da2abfa98
Tardes <Tardes@GET-PROF>
2025-09-16 16:16:37 +0200

    Logback

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,16 +1,15 @@
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import ch.qos.logback.classic.Logger;
 
 public class HolaMundo {
 
-	//private final static Logger log = (Logger)LoggerFactory.getLogger(HolaMundo.class);
+	//private final static Logger log = (Logger)LoggerFactory.getLogger("HolaMundo");
 	
 	public static void main(String args) {
 
-
-		System.out.println("Hola Getafe Jose Luis 9");
-		System.out.println("Test2");
+		//log.info("HOla");
+		//System.out.println("Hola Getafe Jose Luis 9");
+		//System.out.println("Test2");
 
 		//Comentario de línea
 				
--- src/es/cursojava/Test.java
+++ src/es/cursojava/Test.java
@@ -1,11 +1,15 @@
 package es.cursojava;
 
-public class Test {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class Test {
+	private final static Logger log = (Logger)LoggerFactory.getLogger("HolaMundo");
+	
 	public static void main(String[] args) {
 		int num1 = 7;
 		int num2 = 3;
-
+		log.info("HOla");
 		System.out.println(num1/num2);
 		
 		double num11 = 7;
