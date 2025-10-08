832b35988f4a95c52d10789e1bee3f93b70c1eff
Tardes <Tardes@GET-PROF>
2025-10-08 17:32:20 +0200

    Ejemplos Set

---
+++ src/es/cursojava/colecciones/conjuntos/EjemplosSet.java
@@ -0,0 +1,40 @@
+package es.cursojava.colecciones.conjuntos;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import es.cursojava.oo.Persona;
+
+public class EjemplosSet {
+
+	public static void main(String[] args) {
+		Set<Persona> conjunto = new LinkedHashSet();
+//		lista.add("Amarillo");
+//		lista.add(100.5);
+		Persona p = new Persona();
+		conjunto.add(p);
+		for (int i = 0; i < 4; i++) {
+			//boolean aniadido = conjunto.add(p);
+			boolean aniadido = conjunto.add(new Persona(i+"A"));
+			System.out.println(aniadido);
+		}
+
+		for (Persona persona : conjunto) {
+			System.out.println(persona.getDni());
+		}
+		
+		boolean contienep = conjunto.contains(p);
+		System.out.println(contienep);
+		
+		Iterator<Persona> ite = conjunto.iterator();
+		while(ite.hasNext()) {
+			Persona persona = ite.next();
+			System.out.println(persona.getDni());
+//			persona = ite.next();
+//			System.out.println(persona.getDni());
+		}
+		
+	}
+
+}
