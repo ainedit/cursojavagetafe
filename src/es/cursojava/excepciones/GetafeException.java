30b31b5b722119cf1216496b05f78ad5a125dadf
Jose Luis <joseluis@email.com>
2025-11-06 19:06:14 +0100

    Excepciones

---
+++ src/es/cursojava/excepciones/GetafeException.java
@@ -0,0 +1,8 @@
+package es.cursojava.excepciones;
+
+public class GetafeException extends Exception{
+
+	public GetafeException(String mensaje) {
+		super(mensaje);
+	}
+}
---
+++ src/es/cursojava/excepciones/TestExcepcionesPropias.java
@@ -0,0 +1,24 @@
+package es.cursojava.excepciones;
+
+public class TestExcepcionesPropias {
+
+	public static void main(String[] args) {
+		
+		try {
+			metodoTest();
+		} catch (GetafeException e) {
+			System.out.println(e.getMessage());
+		}
+		System.out.println("Termina");
+	}
+
+	public static void metodoTest() throws GetafeException{
+		if (Math.random()>0.2) {
+			GetafeException npe = new GetafeException("Excepción lanzada en Getafe");
+			throw npe;
+			
+		}else {
+			System.out.println("Todo bien");
+		}
+	}
+}
