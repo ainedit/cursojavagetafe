16229aa3785d8db2bb0190cff14de86798ef9e44
Jose Luis <joseluis@email.com>
2025-11-10 18:28:55 +0100

    Ejercicio2 excepciones

---
+++ src/es/cursojava/excepciones/ejercicios/ejercicio2/exceptions/TemperatureException.java
@@ -0,0 +1,10 @@
+package es.cursojava.excepciones.ejercicios.ejercicio2.exceptions;
+
+public class TemperatureException extends Exception {
+
+	public TemperatureException(String message) {
+		super(message);
+	}
+
+	
+}
---
+++ src/es/cursojava/excepciones/ejercicios/ejercicio2/exceptions/TooHotTemperatureException.java
@@ -0,0 +1,9 @@
+package es.cursojava.excepciones.ejercicios.ejercicio2.exceptions;
+
+public class TooHotTemperatureException extends TemperatureException {
+
+	public TooHotTemperatureException(String message) {
+		super(message);
+	}
+	
+}
