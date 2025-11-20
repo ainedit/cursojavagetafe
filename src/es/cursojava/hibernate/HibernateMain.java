c8f25ea26a89de3e38168f9ba6e3b602b808e20d
Jose Luis <joseluis@email.com>
2025-11-20 18:58:12 +0100

    Primer Entity Hibernate

--- src/es/cursojava/hibernate/HibernateMain.java
+++ src/es/cursojava/hibernate/HibernateMain.java
@@ -1,8 +1,13 @@
 package es.cursojava.hibernate;
 
+import java.util.Calendar;
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 public class HibernateMain {
     public static void main(String[] args) {
@@ -12,9 +17,28 @@ public class HibernateMain {
                     .configure() // Carga hibernate.cfg.xml
                     .buildSessionFactory();
             Session session = sessionFactory.openSession();
-            System.out.println(session);
+            System.out.println("Conectado");
                      
+            Transaction tx = session.beginTransaction();
+            
+            Empresa empresa1 = new Empresa("Empresa1","S.A.","123123D","Venta Jabones", 
+            					Calendar.getInstance().getTime());
+            Empresa empresa2 = new Empresa("Empresa2","S.A.","17645D","Venta FunkoPop", 
+            					Calendar.getInstance().getTime());
+            
+            session.persist(empresa1);
+            session.persist(empresa2);
+            
+            tx.commit();
+            
+            List<Empresa> empresas = session.createQuery("from Empresa", Empresa.class).list();
+            for (Empresa empresa : empresas) {
+				System.out.println(empresa);
+			}
+            
+            
         } catch (Throwable ex) {
+        	
             System.err.println("Error al crear la SessionFactory." + ex);
             throw new ExceptionInInitializerError(ex);
         }
