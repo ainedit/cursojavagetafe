0b5ae5228004ef37ac2de04d3ee41b61c7cdb886
Jose Luis <joseluis@email.com>
2025-12-04 20:19:50 +0100

    Hibernate3

--- src/es/cursojava/hibernate/ejercicio1/Ejercicio3Hibernate.java
+++ src/es/cursojava/hibernate/ejercicio1/Ejercicio3Hibernate.java
@@ -17,47 +17,51 @@ public class Ejercicio3Hibernate {
 
     public static void main(String[] args) {
 
-        CursoDAO cursoDAO = new CursoDAOImpl();
-        AulaDAO aulaDAO = new AulaDAO();
         CursoService cursoService = new CursoService();
 
         try {
             // 1. Crear un CursoDTO
-            CursoDTOReq cursoDTO = new CursoDTOReq(
-					"CURSO-JAVA-01",
-					"Java Avanzado con Hibernate",
-					"Aprende conceptos avanzados de Java y Hibernate",
-					Integer.valueOf(40),
-					Boolean.valueOf(true),
-					"Avanzado",
-					"Programación",
-					new BigDecimal(29.99),
-					LocalDate.now(),
-					LocalDate.now().plusMonths(1),
-					LocalDateTime.now()
-			);
-            
-            
-            // 2. Crear un AulaDTO
-            AulaDTO aulaDTO = new AulaDTO(
-                    "A-101",
-                    30,
-                    "Edificio Principal, Planta 1"
-            );
-
-            // 3. Crear curso con aula
-            CursoDTOResp guardado = cursoService.crearCursoConAula(cursoDTO, aulaDTO);
-
-            System.out.println("Curso guardado con ID: " + guardado.getId());
-            System.out.println("Aula del curso: " +
-                    (guardado.getAula() != null ? guardado.getAula().getCodigoAula() : "Sin aula"));
-
+//            CursoDTOReq cursoDTO = new CursoDTOReq(
+//					"CURSO-JAVA-01",
+//					"Java Avanzado con Hibernate",
+//					"Aprende conceptos avanzados de Java y Hibernate",
+//					Integer.valueOf(40),
+//					Boolean.valueOf(true),
+//					"Avanzado",
+//					"Programación",
+//					new BigDecimal(29.99),
+//					LocalDate.now(),
+//					LocalDate.now().plusMonths(1),
+//					LocalDateTime.now()
+//			);
+//            
+//            
+//            // 2. Crear un AulaDTO
+//            AulaDTO aulaDTO = new AulaDTO(
+//                    "A-101",
+//                    30,
+//                    "Edificio Principal, Planta 1"
+//            );
+//
+//            // 3. Crear curso con aula
+//            CursoDTOResp guardado = cursoService.crearCursoConAula(cursoDTO, aulaDTO);
+//
+//            System.out.println("Curso guardado con ID: " + guardado.getId());
+//            System.out.println("Aula del curso: " +
+//                    (guardado.getAula() != null ? guardado.getAula().getCodigoAula() : "Sin aula"));
+//
             // 4. Recuperar el curso de BD
-            CursoDTOResp recuperado = cursoService.buscarPorId(guardado.getId());
+            CursoDTOResp recuperado = cursoService.buscarPorId(42L);
+            System.out.println("Curso recuperado ID 42");
             System.out.println("Curso recuperado: " + recuperado.getNombre());
             System.out.println("Aula asociada: " + recuperado.getAula().getCodigoAula());
 
-        } finally {
+        }catch(IllegalArgumentException e) {
+			System.err.println("Error de validación: " + e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        finally {
             System.out.println("Finalizando aplicación y cerrando sesión Hibernate.");
         }
     }
